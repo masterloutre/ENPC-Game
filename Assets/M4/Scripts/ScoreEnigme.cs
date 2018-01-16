1a2ff39a7c2f54ff0893325f7e0f0b4ca325ef64
JustineVuillemot <vuillemotjustine@gmail.com>
2018-01-16 15:13:18 +0100

    scene for saving test created and classes for the json object

---
+++ Assets/M4/Scripts/ScoreEnigme.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreEnigme{
+
+    public string nom;
+    public string []compétences;
+    public string situationPro;
+    public int type;
+    public int difficulté;
+    public int points;
+    public int tentatives;
+    public int temps;
+    public bool aideExt;
+    public int etudiant;
+
+}
