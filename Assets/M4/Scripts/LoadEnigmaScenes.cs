c5d19375285bc2c27f20e0681d973b0a89a3251f
nornei <noemie.pije@gmail.com>
2018-01-19 00:52:38 +0100

    create default scene from which enigmas can be loaded

---
+++ Assets/M4/Scripts/LoadEnigmaScenes.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadEnigmaScenes : MonoBehaviour {
+
+    public int sceneToLoad;
+    public string sceneName;
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 80, 200, 50), "Current Scene: " + (SceneManager.GetActiveScene().name));
+        if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height -200, 200, 50), "Load Scene " + (sceneName)))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
