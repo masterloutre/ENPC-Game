ce2769ae7cb371a515f2138719ab527a0a727400
masterloutre <williejunpow@gmail.com>
2018-06-21 15:25:49 +0200

    input enigma validation works

--- Assets/InputValidation.cs
+++ Assets/InputValidation.cs
@@ -1,19 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using B83.ExpressionParser;
 
-public class InputValidation : MonoBehaviour
+public class InputValidation : MonoBehaviour, ValidationMethod
 {
+	public string formula;
+	public double marginError;
+	List<double> param;
+	ExpressionParser parser;
 
-	// Use this for initialization
+
 	void Start ()
 	{
-	
+		GameObject[] paramGOList = GameObject.FindGameObjectsWithTag ("Input Param");
+		parser = new ExpressionParser();
+		param = new List<double> ();
+		foreach(GameObject paramGO in paramGOList){
+			param.Add(double.Parse (paramGO.GetComponent<UnityEngine.UI.Text> ().text));
+		}
 	}
 	
-	// Update is called once per frame
-	void Update ()
-	{
-	
+	public bool answerIsRight(){
+		string studentInput = GameObject.FindGameObjectWithTag("Input Text").GetComponentInChildren<UnityEngine.UI.Text> ().text;
+		Expression expCorrect = parser.EvaluateExpression(formula);
+		if(studentInput != ""){
+			float studentAnswer = float.Parse(studentInput); 
+			setParam (expCorrect);
+
+			float error = (float)(studentAnswer - expCorrect.Value);
+
+			if (Mathf.Abs (error) <= marginError) {
+				Debug.Log ("Bonne réponse : ");
+				return true;
+			}
+			else {
+				Debug.Log ("Mauvaise réponse : "+studentAnswer+" " + expCorrect.Value);
+				return false;
+			}
+		} 
+		return false;
+	}
+
+	void setParam(Expression expCorrect){ //set param in alphabetical order
+		for (int i = 0; i < param.Count; i++) {
+			char tmp = (char)('a' + i);
+			string id = "" + tmp;
+			expCorrect.Parameters[id].Value = param[i]; // set the named parameter 
+		}
 	}
 }
 
