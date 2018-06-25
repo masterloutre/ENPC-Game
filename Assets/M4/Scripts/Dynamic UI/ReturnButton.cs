2753aae14e5c1a7eb885f98656b7eec4dc700562
masterloutre <williejunpow@gmail.com>
2018-06-25 15:28:18 +0200

    documentation scene managers

--- Assets/M4/Scripts/Dynamic UI/ReturnButton.cs
+++ Assets/M4/Scripts/Dynamic UI/ReturnButton.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/*
+ * Component qui permet de retourner au menu précédent
+ */ 
+
 public class ReturnButton : MonoBehaviour {
 
+	//Envoie un event de type RequestPreviousSceneEvent
 	public void PreviousScene(){
+		//Traité dans GlobalManager
 		EventManager.instance.Raise (new RequestPreviousSceneEvent(SceneManager.GetActiveScene().name, 0));
 	}
 }
