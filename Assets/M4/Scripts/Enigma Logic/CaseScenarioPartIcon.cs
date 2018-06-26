8722ccf7f853509540ade3e49b4d312033cffabb
masterloutre <williejunpow@gmail.com>
2018-06-26 16:19:19 +0200

    starting case study scenario enigma

---
+++ Assets/M4/Scripts/Enigma Logic/CaseScenarioPartIcon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaseScenarioPartIcon {
+	private GameObject prefab;
+	private CaseScenarioPart scenarioPart;
+	// Use this for initialization
+
+	public CaseScenarioPartIcon(CaseScenarioPart _scenarioPart, GameObject _prefab){
+		scenarioPart = _scenarioPart;
+		prefab = _prefab;
+	}
+}
