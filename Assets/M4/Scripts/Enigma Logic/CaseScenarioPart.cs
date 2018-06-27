3ed6604e51abdf9b844d42f76b63b31068abe359
masterloutre <williejunpow@gmail.com>
2018-06-27 11:15:49 +0200

    displaying icons ok

--- Assets/M4/Scripts/Enigma Logic/CaseScenarioPart.cs
+++ Assets/M4/Scripts/Enigma Logic/CaseScenarioPart.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CaseScenarioPart : MonoBehaviour {
-
+	public int id { get; private set; }
+	public GameObject iconPrefab;
 	// Use this for initialization
 	void Start () {
-		
+		id = Array.IndexOf(transform.parent.GetComponentsInChildren<CaseScenarioPart> (), this);
+		CaseScenarioPartIcon icon = new CaseScenarioPartIcon (this, iconPrefab);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void show(){
+		foreach (CaseScenarioPart scenarioPart in transform.parent.GetComponentsInChildren<CaseScenarioPart>()) {
+			scenarioPart.hide ();
+		}
+		gameObject.SetActive (true);
+	}
+
+	public void hide(){
+		gameObject.SetActive (false);
 	}
 }
--- Assets/M4/Scripts/Enigma Logic/CaseScenarioPartIcon.cs
+++ Assets/M4/Scripts/Enigma Logic/CaseScenarioPartIcon.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class CaseScenarioPartIcon {
 	private GameObject prefab;
@@ -10,5 +12,22 @@ public class CaseScenarioPartIcon {
 	public CaseScenarioPartIcon(CaseScenarioPart _scenarioPart, GameObject _prefab){
 		scenarioPart = _scenarioPart;
 		prefab = _prefab;
+
+		createGameObject ();
+
 	}
+
+	public void activateScenarioPart(){
+		scenarioPart.show ();
+	}
+
+	public void createGameObject(){
+		float offsetY = scenarioPart.id * (-30) - 30;
+		GameObject parentGO = GameObject.Find ("TimeLine");
+
+		GameObject iconGO = GameObject.Instantiate(prefab, parentGO.transform, false);
+		iconGO.transform.position += new Vector3 (0, offsetY, 0);
+		iconGO.GetComponent<UnityEngine.UI.Button> ().onClick.AddListener (activateScenarioPart);
+	}
+
 }
