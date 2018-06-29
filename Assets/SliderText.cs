5347f94c727c89496575a81da5701eeca3134a4d
masterloutre <williejunpow@gmail.com>
2018-06-29 09:54:34 +0200

    slider displays value on change

---
+++ Assets/SliderText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+	public Text sliderText;
+	
+	public void displaySliderValue(){
+		int value = (int)gameObject.GetComponent<Slider>().value;
+		sliderText.text = value.ToString();
+	}
+}
