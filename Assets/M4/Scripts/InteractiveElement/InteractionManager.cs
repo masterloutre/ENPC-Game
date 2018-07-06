db4ce5e2cbe9a1820347612eb0e6a3b97e2a1a66
Kenthyvong Tan <kenthyvong.tan@gmail.com>
2018-07-06 15:12:03 +0200

    fixed known bug where event trigger component would eat scrolling event => created a separate event system to custom interaction, making it case by case => can know scroll on skill selection menu while zooming over items

--- Assets/M4/Scripts/InteractiveElement/InteractionManager.cs
+++ Assets/M4/Scripts/InteractiveElement/InteractionManager.cs
@@ -4,40 +4,21 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InteractionManager : MonoBehaviour, InteractiveUI {
-
-
-	float coeffHover = 1.5f;
+public class InteractionManager : InteractiveUI{
+    
+    
 
 	public void InfoChangeState(GameObject go){
 		go.SetActive (!go.activeSelf);
 	}
     
-    public void zoomOnHover(GameObject go){
-        Vector3 res = go.transform.localScale;
-        res.Scale(new Vector3(coeffHover, coeffHover, coeffHover));
-        go.transform.localScale=res;
-
-	}
-
-	public void dezoom(GameObject go){
-        Vector3 res = go.transform.localScale;
-        res.Scale(new Vector3(1/coeffHover, 1/coeffHover, 1/coeffHover));
-        go.transform.localScale = res;
-    }
-
-	public void exitGameButton(){
+    
+    public void exitGameButton(){
         // ne fonctionne pas sur l'éditeur, uniquement en application
 		Application.Quit ();
 	}
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        zoomOnHover(gameObject);
-    }
+    
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        throw new System.NotImplementedException();
-    }
+    
 }
