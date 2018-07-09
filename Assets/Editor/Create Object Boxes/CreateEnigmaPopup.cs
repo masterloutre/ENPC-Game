4a1076a8c72ceb7e50848177e7e1ce69653bf1cb
masterloutre <williejunpow@gmail.com>
2018-07-09 16:38:05 +0200

    some Doc and more tests

--- Assets/Editor/Create Object Boxes/CreateEnigmaPopup.cs
+++ Assets/Editor/Create Object Boxes/CreateEnigmaPopup.cs
@@ -6,49 +6,55 @@ using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 using System.Linq;
 
+
+/**
+ * Pop up permettant de créer une énigme.
+ * La scène de l'énigme est créée avec l'id entré par l'utilisateur (attention à ne pas écraser une autre énigme) dans le dossier Enigmes
+ * Elle est directment ouverte dans l'éditeur et ajoutée à la liste des builds
+ * @type {[type]}
+ */
 public class CreateEnigmaPopup : PopupWindowContent
 {    
+    //type d'énigme
     EnigmaType type;
-    int id = 7;
+    //index unity de l'énigme (doit correspondre à l'index entré dans la bdd)
+    int id = 0;
+    
+    
     public CreateEnigmaPopup(EnigmaType _type){
       type = _type;
-      
     }
   
-  
-    public override Vector2 GetWindowSize()
-    {
+    //taille de la fenetre de pop up
+    public override Vector2 GetWindowSize()  {
         return new Vector2(500, 400);
     }
 
+    //affichage des champs et bouttons et assignement des variables
     public override void OnGUI(Rect rect)
     {
         GUILayout.Label("Enigme : " + type, EditorStyles.boldLabel);
         id = EditorGUILayout.IntField("ID de l'énigme\n(doit correspondre à l'index unity entré en base de donnée)", id);
-        GUILayout.Label("Cette fonctionnalité n'est pas encore disponible", EditorStyles.boldLabel);
+        GUILayout.Label("Attention si vous entrez un ID déjà utilisé vous allez remplacer l'énigme lui étant associé par un template vierge!\n Pensez à vérifier l'ID avant de valider.", EditorStyles.boldLabel);
         if (GUILayout.Button("Créer l'énigme", GUILayout.Width(200))) {
 		      createEnigmaScene();
 		    }
       
     }
-
-    public override void OnOpen()
-    {
-        Debug.Log("Popup opened: " + this);
-    }
-
-    public override void OnClose()
-    {
-        Debug.Log("Popup closed: " + this);
-    }
     
+    //création de la scène d'énigme
     public void createEnigmaScene(){
+      //chemin du template (en fct du type d'énigme)
       string path = "Assets/Scenes/Templates Enigmes/Enigme "+type+".unity";
+      //chemin de l'énigme crée (en fct de l'ID renseigné)
       string newPath = "Assets/Scenes/Enigmes/Enigma" + id+ ".unity";
+      //copy du template au nouveau chemin
       AssetDatabase.CopyAsset(path, newPath);  
+      //Ajout de la nouvelle scène au build settings
       List<EditorBuildSettingsScene> buildList = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
       buildList.Add(new EditorBuildSettingsScene(newPath, true));
       EditorBuildSettings.scenes = buildList.ToArray();
+      //Ouverture de la nouvelle scène dans l'éditeur
       Scene newEnigmaScene = EditorSceneManager.OpenScene(newPath,  OpenSceneMode.Single);
     }
 }
--- Assets/Editor/Create Object Boxes/CreateLegendPopup.cs
+++ Assets/Editor/Create Object Boxes/CreateLegendPopup.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 using UnityEditor;
 using System;
 
+
+/**
+ * Popup permettant d'ajouter une légende ou une donnée dans une énigme
+ * Le gameObject est créé d'apres le prefab InteractiveValue et peut ou non être déterminant pour le calcul du résultat (Input Param)
+ * Il est placé dans le groupe Légende de la scène actuelle 
+ * @type {[type]}
+ */
 public class CreateLegendPopup : PopupWindowContent
 {
 	string name = "LegendLongeur";
@@ -14,59 +21,61 @@ public class CreateLegendPopup : PopupWindowContent
 	bool jump = false;
 	bool inputValue = false;
 	
-
-    public override Vector2 GetWindowSize()
-    {
+		//taille du popup
+    public override Vector2 GetWindowSize(){
         return new Vector2(250, 250);
     }
 
-    public override void OnGUI(Rect rect)
-    {
+		///affichage des champs et bouttons et assignement des variables
+    public override void OnGUI(Rect rect){
         GUILayout.Label("Ajouter une légende", EditorStyles.boldLabel);
+				//description de la légende
 				name = EditorGUILayout.TextField("Nom de l'object", name);
 				value = EditorGUILayout.FloatField("Valeur", value);
 				unit = EditorGUILayout.TextField("Unité", unit);
 				legend = EditorGUILayout.TextField("Texte de légende", legend);
 				jump = EditorGUILayout.Toggle("Aller à la ligne", jump);
+				//input value ou pas
 				inputValue = EditorGUILayout.Toggle("Sert au calcul du résultat", inputValue);
 				if(inputValue){
 					variableName =  EditorGUILayout.TextField("Nom de la variable", "x");
 				} else {
 					variableName = "";
 				}
+				//bouton OK
 				if (GUILayout.Button("OK", GUILayout.Width(200))) {
 		      createLegendGameObject();
 		    }
         
     }
-
-    public override void OnOpen()
-    {
-        Debug.Log("Popup opened: " + this);
-    }
-
-    public override void OnClose()
-    {
-        Debug.Log("Popup closed: " + this);
-    }
 		
+		//Crée un GameObject à partir du prefab InteractiveValue et le place dans le groupe Légendes de la scène actuellement ouverte
+		//Si plusieurs scènes d'énigmes sont ouvertes ils seront placé dans le premier groupe Légende trouvé
 		public void createLegendGameObject(){
+			//création du gameObject
 			GameObject legendGO = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath("Assets/M4/Prefabs/Enigmas/interactive value.prefab", typeof(GameObject)));
 			legendGO.name = name;
 			
+			//remplissage du component InteractiveValue
 			InteractiveValue iv = legendGO.GetComponent<InteractiveValue>();
 			iv.valeur = value;
 			iv.unité = unit;
 			iv.légende = legend;
 			iv.nomDeVariable = (variableName == "")? ' ':variableName[0];
 			iv.valeurÀLaLigne = jump;
+			
+			//Ajout du tag (nécessaire pour la validation de l'énigme INPUT)
 			if(inputValue){
 				legendGO.tag = "Input Param";
+			} else {
+				legendGO.tag = "Untagged";
 			}
 			
+			//Positionnement dans la hierarchie de la scène
 			GameObject parent = GameObject.Find("Légendes");
-			legendGO.transform.SetParent(parent.transform, false);
-			Debug.Log("Legend created : " + legendGO.name + ", parent : " + parent.name);
+			if(parent){
+				legendGO.transform.SetParent(parent.transform, false);
+			}	
 		}
 		
 }
