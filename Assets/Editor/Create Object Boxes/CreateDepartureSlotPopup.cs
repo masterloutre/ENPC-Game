2cdf042a8042ee92928c23bbbd6ee0f1b3a287fc
masterloutre <williejunpow@gmail.com>
2018-07-11 16:12:01 +0200

    adding qcm slots OK

---
+++ Assets/Editor/Create Object Boxes/CreateDepartureSlotPopup.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+
+/**
+ * Popup permettant d'ajouter un slot de qcm de départ
+ * Le gameObject est créé d'apres le prefab Departure Slot, ce prefab possède un composant Item nécessaire à la validation de l'énigme
+ * Il est placé dans le groupe Departure Slots de la scène actuelle
+ * @type {[type]}
+ */
+public class CreateDepartureSlotPopup : PopupWindowContent
+{
+	string name = "DepartureSlot";
+	int id = 0;
+	float value = 0;
+	string unit = "cm";
+	string legend = "";
+	bool jump = false;
+
+
+		//taille du popup
+    public override Vector2 GetWindowSize(){
+        return new Vector2(250, 250);
+    }
+
+		///affichage des champs et bouttons et assignement des variables
+    public override void OnGUI(Rect rect){
+        GUILayout.Label("Ajouter un slot de départ", EditorStyles.boldLabel);
+				name = EditorGUILayout.TextField("Nom de l'object", name);
+
+				id =  EditorGUILayout.IntField("Identifiant", id);
+
+				value = EditorGUILayout.FloatField("Valeur", value);
+				unit = EditorGUILayout.TextField("Unité", unit);
+				legend = EditorGUILayout.TextField("Texte de légende", legend);
+				jump = EditorGUILayout.Toggle("Aller à la ligne", jump);
+
+				//bouton OK
+				if (GUILayout.Button("OK", GUILayout.Width(200))) {
+		      createLegendGameObject();
+		    }
+
+    }
+
+		//Créer l'objet et le place dans le groupe Departure Slots
+		public void createLegendGameObject(){
+			//création du gameObject
+			GameObject slotGO = (GameObject)PrefabUtility.InstantiatePrefab(AssetDatabase.LoadAssetAtPath("Assets/M4/Prefabs/Enigmas/qcm/Departure Slot.prefab", typeof(GameObject)));
+			slotGO.name = name;
+
+			//remplissage du component InteractiveValue
+			InteractiveValue iv = slotGO.GetComponentInChildren<InteractiveValue>();
+			iv.valeur = value;
+			iv.unité = unit;
+			iv.légende = legend;
+			iv.nomDeVariable = ' ';
+			iv.valeurÀLaLigne = jump;
+
+			//remplissage du component item
+			Item item = slotGO.GetComponentInChildren<Item>();
+			item.item_id = id;
+
+			//Positionnement dans la hierarchie de la scène
+			GameObject parent = GameObject.Find("Departure Slots");
+			if(parent){
+				slotGO.transform.SetParent(parent.transform, false);
+			}
+		}
+
+}
--- Assets/Editor/Create Object Boxes/CreateQCMEnigmaEditor.cs
+++ Assets/Editor/Create Object Boxes/CreateQCMEnigmaEditor.cs
@@ -5,23 +5,35 @@ using UnityEditor;
 
 public class CreateQCMEnigmaEditor : CreateEnigmaEditor
 {
-  
+
   [MenuItem("Creation Enigme/Type QCM")]
   static void Init()
   {
     EditorWindow window = EditorWindow.CreateInstance<CreateQCMEnigmaEditor>();
     window.Show();
   }
-	
+
   void OnGUI()
   {
     GUILayout.Label("Creation d'énigme de type QCM", EditorStyles.boldLabel);
-		
-		if (GUILayout.Button("Créer une énigme depuis un fichier", GUILayout.Width(200))) {
-				showPopupContentAtCenter(new CreateEnigmaFromFilePopup(EnigmaType.QCM));
+
+		if (GUILayout.Button("Créer une énigme", GUILayout.Width(250))) {
+				showPopupContentAtCenter(new CreateEnigmaPopup(EnigmaType.INPUT));
     }
-    if (GUILayout.Button("Ajouter une légende", GUILayout.Width(200))) {
+    if (GUILayout.Button("Ajouter une légende", GUILayout.Width(250))) {
         showPopupContentAtCenter(new CreateLegendPopup());
+    }
+    if (GUILayout.Button("Ajouter un schéma", GUILayout.Width(250))) {
+        showPopupContentAtCenter(new CreateDiagramPopup());
+    }
+    if (GUILayout.Button("Ajouter un slot de départ", GUILayout.Width(250))) {
+        showPopupContentAtCenter(new CreateDepartureSlotPopup());
+    }
+    if (GUILayout.Button("Ajouter un slot d'arrivée", GUILayout.Width(250))) {
+        showPopupContentAtCenter(new CreateDestinationSlotPopup());
+    }
+    if (GUILayout.Button("Modifier les questions de méthode", GUILayout.Width(250))) {
+        showPopupContentAtCenter(new SetMethodQuestionsPopup());
     }
   }
 }
