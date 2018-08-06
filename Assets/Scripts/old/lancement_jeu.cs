3ef2ccb501004cb197ff09c40d733a61542b394e
masterloutre <williejunpow@gmail.com>
2018-08-06 11:10:57 +0200

    clean scripts

--- Assets/Scripts/old/lancement_jeu.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class lancement_jeu {
-    //public int id;
-    //public int phase;
-    public string mdp;
-}
